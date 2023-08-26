480d707c8fb4fbac08378acdc2a218b590025a74
Luis Pizarro <lpizarroavila@gmail.com>
2023-08-26 02:24:38 -0400

    implementacion de filtros y boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,76 @@ import useApi from './hooks/useApi'
 import { global } from './variables/global'
 import InfoChar from './components/InfoChar'
 
+import { Button, Form } from 'react-bootstrap'
+
 function App() {
   const toggle = useToggle(false)
   const { API_URL_CHARACTER } = global
   const { data, active, consume } = useApi(API_URL_CHARACTER)
   const [info, setInfo] = useState(null)
+  const [backUp, setBackUp] = useState([])
   const [page, setPage] = useState({})
+  const [especie, setEspecie] = useState([])
+  const [status, setStatus] = useState([])
+  const [selStatus, setSelStatus] = useState()
+  const [selEspecie, setSelEspecie] = useState()
+  const [mensaje, setMensaje] = useState('')
 
   useEffect(() => {
-    if(info==null){
+    if (info == null) {
       apiCall(API_URL_CHARACTER)
     }
   }, [active])
+
+// filtrar según el estado y especie
+  function filter() {
+    let aux = backUp
+    let array = aux.filter(data => data.status == selStatus)
+      .filter(data => data.species == selEspecie)
+    if (array.length == 0) {
+      setMensaje('No se encontraron coincidencias')
+    }
+    setInfo(array)
+  }
+  // limpiar filtros
+  function limpiar(){    
+    setInfo(backUp)
+    setSelEspecie('')
+    setSelStatus('')
+  }
+
+  // obtener la especie 
+  function spec(data) {
+    let aux = []
+    data.map(data => {
+      aux.push(data.species)
+    })
+    // Set solo permite almacenar valores únicos
+    const limpio = new Set(aux)
+    let result = [...limpio]
+    setEspecie(result.sort())
+  }
+
+  function findStatus(data) {
+    let aux = []
+    data.map(data => {
+      aux.push(data.status)
+    })
+    // Set solo permite almacenar valores únicos
+    const limpio = new Set(aux)
+    let result = [...limpio]
+    setStatus(result.sort())
+  }
 
   // LLamada por defecto API    
   async function apiCall(url) {
     toggle.toggleFalse()
     await consume(url)
     if (!active) {
+      spec(data.results)
+      findStatus(data.results)
       setInfo(data.results.sort((a, b) => a.name.localeCompare(b.name)))
+      setBackUp(data.results.sort((a, b) => a.name.localeCompare(b.name)))
       setPage({
         next: data.info.next,
         prev: data.info.prev
@@ -33,47 +84,74 @@ function App() {
     }
   }
 
-// Funcion pagina siguiente
-  async function siguiente(){
+  // Función pagina siguiente
+  async function siguiente() {
     toggle.toggleFalse()
     await fetch(page.next)
-    .then(res=>res.json())
-    .then(data=> {
-      setInfo(data.results.sort((a, b) => a.name.localeCompare(b.name)))
-      setPage({
-        next: data.info.next,
-        prev: data.info.prev
+      .then(res => res.json())
+      .then(data => {
+        spec(data.results)
+
+        findStatus(data.results)
+        setInfo(data.results.sort((a, b) => a.name.localeCompare(b.name)))
+        setBackUp(data.results.sort((a, b) => a.name.localeCompare(b.name)))
+        setPage({
+          next: data.info.next,
+          prev: data.info.prev
+        })
+        toggle.toggleTrue()
       })
-      toggle.toggleTrue()    
-    })
   }
 
-  // Funcion pagina anterior
-  async function anterior(){
+  // Función pagina anterior
+  async function anterior() {
     toggle.toggleFalse()
     await fetch(page.prev)
-    .then(res=>res.json())
-    .then(data=> {
-      setInfo(data.results.sort((a, b) => a.name.localeCompare(b.name)))
-      setPage({
-        next: data.info.next,
-        prev: data.info.prev
+      .then(res => res.json())
+      .then(data => {
+        spec(data.results)
+
+        findStatus(data.results)
+        setInfo(data.results.sort((a, b) => a.name.localeCompare(b.name)))
+        setBackUp(data.results.sort((a, b) => a.name.localeCompare(b.name)))
+        setPage({
+          next: data.info.next,
+          prev: data.info.prev
+        })
+        toggle.toggleTrue()
       })
-      toggle.toggleTrue()    
-    })
   }
 
   return (
     <div>
+      <Form.Select value={selStatus} onChange={(e) => setSelStatus(e.target.value)} aria-label='Status'>
+        <option>Status</option>
+        {
+          status.map((data, index) =>
+            <option key={index} value={data}>{data}</option>
+          )
+        }
+      </Form.Select>
+      <Form.Select value={selEspecie} onChange={(e) => setSelEspecie(e.target.value)} aria-label='Species'>
+        <option>Species</option>
+        {
+          especie.map((data, index) =>
+            <option key={index} value={data}>{data}</option>
+          )
+        }
+      </Form.Select>
+      <Button onClick={() => filter()}>Buscar</Button>
+      <Button onClick={() => limpiar()}>Limpiar</Button>
+
       {
         page.prev ? (
           <button onClick={(e) => anterior()}>Anterior</button>
         ) : null
       }
       {
-        page.next?(
+        page.next ? (
           <button onClick={(e) => siguiente()}>Siguiente</button>
-        ):null
+        ) : null
       }
       {
         toggle.active ? (
