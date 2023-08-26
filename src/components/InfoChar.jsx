a6bd8a6b2fc9a85ab0ccc268feef9f707bd31966
Luis Pizarro <lpizarroavila@gmail.com>
2023-08-26 03:47:00 -0400

    implementacion detalle del personaje y sass

--- src/components/InfoChar.jsx
+++ src/components/InfoChar.jsx
@@ -1,11 +1,46 @@
+import { Button, Card, Modal, ModalFooter } from "react-bootstrap"
+import useToggle from "../hooks/useToggle"
+
 const InfoChar = (props) => {
   const {data}=props
+  console.log(props)
+  const {active, toggleFalse, toggleTrue} = useToggle(false)
   return (
     <div>
-      <div>Nombre: {data.name}</div>
-      <div>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={data.image} />
+      <Card.Body>
+        <Card.Title>{data.name}</Card.Title>
+        <Button variant="primary" onClick={()=>toggleTrue()}>Show more...</Button>
+      </Card.Body>
+    </Card>
+    {
+      active && 
+      <Modal
+      show={active}
+      onHide={() => toggleFalse()}
+      dialogClassName="modal-90w"
+      aria-labelledby="example-custom-modal-styling-title"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="example-custom-modal-styling-title">
+          {data.name}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
         <img src={data.image} alt={data.name} />
-      </div>
+        
+      </Modal.Body>
+      <ModalFooter>
+        Species: {data.species}
+        Status: {data.status}
+        Gender: {data.gender}
+        Origin: {data.origin.name}
+        Location: {data.location.name}
+      </ModalFooter>
+    </Modal>
+    }
+    
     </div>
   )
 }
